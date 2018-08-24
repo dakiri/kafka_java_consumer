8f1d1e22056d157705c6cbd7110e33fb6dd673ae
Eric RICHARD <erichard@adista.fr>
2018-08-24 16:54:46 +0200

    Adding command line

--- src/main/java/doubloon/ProcessingApp.java
+++ src/main/java/doubloon/ProcessingApp.java
@@ -2,30 +2,50 @@ package doubloon;
 
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import java.lang.System;
+import java.lang.Runtime;
 
 public class ProcessingApp {
 
    public static void main(String[] args){
-     new ProcessingApp();
+     if (args.length!=3) {
+        System.out.println("Usage servers groupId topic");
+	System.out.println("java -jar ./build/libs/java-0.1.0.jar 10.23.2.73:9092 testJava2 syslog");
+     }
+     else {
+	new ProcessingApp(args[0],args[1],args[2]);
+     }
    }
 
-   ProcessingApp() {
+   ProcessingApp(String servers,String groupId,String topic) {
 
-        System.out.println("Hello, World");
-	String servers ="10.23.2.73:9092";
-	String groupId ="testJava2";
-	String topic="syslog";
+	long count=0;
 
-        System.out.println("Server : "+servers);
-
+        System.out.println("Connecting to server : "+servers);
         Reader reader = new Reader(servers,groupId,topic);
+
+        final long startTime = System.currentTimeMillis();
+
+	Runtime.getRuntime().addShutdownHook(new Thread() {
+
+		public void run() {
+                    long stopTime = System.currentTimeMillis();
+                    System.out.println("Execution time : "+(stopTime-startTime)+ "ms");
+
+		}
+	    });
+
+
 
 	while (true) {
    		ConsumerRecords<String, String> consumerRecords = reader.consume();
 
                 System.out.println("We got record count " + consumerRecords.count());
+                count+= consumerRecords.count();
+                System.out.println("Total " + count);
+
 		for (ConsumerRecord<String,String> record : consumerRecords){
-//			System.out.println("_"+record.value());
+//			System.out.println(record.value());
 		}
 
 	}
--- src/main/java/doubloon/Reader.java
+++ src/main/java/doubloon/Reader.java
@@ -4,19 +4,21 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class Reader implements Consumer {
-private final KafkaConsumer<String, String> consumer;
-private final String topic;
 
-public Reader(String servers, String groupId, String topic) {
-   this.consumer=new KafkaConsumer<String,String>(Consumer.createConfig(servers,groupId));
-   this.topic=topic;
-}
+	private final KafkaConsumer<String, String> consumer;
+	private final String topic;
+
+	public Reader(String servers, String groupId, String topic) {
+		this.consumer=new KafkaConsumer<String,String>(Consumer.createConfig(servers,groupId));
+		this.topic=topic;
+	}
 
 
-public ConsumerRecords<String, String> consume() {
-this.consumer.subscribe(java.util.Arrays.asList(this.topic));
-ConsumerRecords<String,String> records = consumer.poll(100);
-return records;
-}
+	public ConsumerRecords<String, String> consume() {
+		this.consumer.subscribe(java.util.Arrays.asList(this.topic));
+                // Wait 100 milliseconds if data is not available in the buffer
+		ConsumerRecords<String,String> records = consumer.poll(100);
+		return records;
+	}
 
 }
